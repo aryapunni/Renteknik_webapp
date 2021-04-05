2d0a402bb6a005d09bde8a7dccb64b1af21010e2
arya <arya@arya.renteknikgroup>
2021-04-05 14:35:06 -0400

    adding /panpower get and post function. Get function displays posted data

--- application.py
+++ application.py
@@ -5,6 +5,30 @@ import sqlalchemy
 app = FastAPI()
 
 
+panpower = {}
+
+
+# panpower post function
+@app.post("/panpower")
+async def post_panpower(datain: dict):
+    global panpower
+    panpower = datain
+    return 1
+
+
+# panpower display function
+@app.get("/panpower")
+async def get_panpower():
+    return {"panpower": panpower}
+
+
+# Path parameter test function
+@app.get("/item/{item_id}")
+async def item(item_id: int):
+    return {"item_id": item_id}
+
+
+# FastAPI initial test function
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
