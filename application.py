33f4152b7cb356d3ca47a8c7613723c3bc1b9f9b
arya <arya@arya.renteknikgroup>
2021-04-09 14:49:28 -0400

    adding schemas, added schema for measurements

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 
 from fastapi import FastAPI
 import sqlalchemy
+from typing import Optional, List
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -8,12 +10,33 @@ app = FastAPI()
 panpower = {}
 
 
+# Schema for panpower measurements
+class PanpowerMeasurement(BaseModel):
+    device_id: int = Field(..., alias='device_id')
+    device_name: str = Field(..., alias='device_name')
+    measurement_time: str = Field(..., alias='measurement_time(UTC)')
+    resolution: int = Field(..., alias='resolution(minutes)')
+    site_id: int = Field(..., alias='site_id')
+    site_name: str = Field(..., alias='site_name')
+    current: float = Field(..., alias='current(A)')
+    voltage: float = Field(..., alias='voltage(V)')
+    power: float = Field(..., alias='power(W)')
+    power_factor: float = Field(..., alias='power_factor')
+    energy: float = Field(..., alias='energy(Wh)')
+
+
+# schema for array of measurements
+class PanpowerDictCover(BaseModel):
+    measurements: List[PanpowerMeasurement]
+
+
+
 # panpower post function
-@app.post("/panpower")
-async def post_panpower(datain: dict):
+@app.post("/panpower/measurements")
+async def panpower_format(datain: PanpowerDictCover):
     global panpower
     panpower = datain
-    return 1
+    return datain
 
 
 # panpower display function
