aeb4c4fb7826f23c7b877e3dfc8bf8e61dc52378
arya <arya@arya.renteknikgroup>
2021-09-28 16:20:06 -0400

    testing going on

--- application.py
+++ application.py
@@ -108,13 +108,29 @@ async def get_panpowerpulse(client: str, db: Session = Depends(get_db)):
 # Arc data posting link
 @app.post("/arc/consumption/{client}/{leed_id}/{meter_id}")
 async def post_consumption(meter_id: str, leed_id: str, client: str, datain: schemas.ArcEnergyDictCover, db: Session = Depends(get_db)):
+
+    # Fetch data from the data base based on the leed id
     meta_data = crud.get_arc_metadata_leedid(db, leed_id)
+
+    # If that leed id is not available in the database
+    # Send a 404 error
+    if meta_data is None:
+        print("There is no such Project in that Leed ID")
+        raise HTTPException(status_code=404, detail="Leed ID not found")
+
+    # If the data fetching was successfull proceed to send data to Arc
     for data in datain.measurements:
         data.meter_id = meter_id
         data.leed_id = leed_id
         data.client = client
+
+    # Electrical hierarchy for filtering data
     electrical_hierarchy = meta_data.electrical_hierarchy
+
+    # Timezone and time duration information for processing data
     time_data = {"duartion_format": meta_data.duration_format, "duration": meta_data.duration, "time_zone": meta_data.timezone}
+
+    # Send data to Arc
     send_arc_consumption(db, datain.dict(), electrical_hierarchy, time_data)
     return 200
 
@@ -122,8 +138,11 @@ async def post_consumption(meter_id: str, leed_id: str, client: str, datain: sch
 # Arc create meter in ARC
 @app.post("/arc/create_meter")
 async def create_arc_meter(datain: schemas.ArcCreateMeter, db: Session = Depends(get_db)):
-    # print(datain)
-    arc.arc_post.create_meter_object(db=db, leed_id=datain.leed_id, client_name=datain.client_name, meter_type=datain.meter_type, unit=datain.meter_unit, meter_id=datain.meter_id, name=datain.meter_name, partner_details=datain.renteknik_meter)
+
+    # Create a meter in the specific leed ID
+    arc.arc_post.create_meter_object(db=db, leed_id=datain.leed_id, client_name=datain.client_name, \
+                                     meter_type=datain.meter_type, unit=datain.meter_unit, meter_id=datain.meter_id, \
+                                     name=datain.meter_name, partner_details=datain.renteknik_meter)
     return 200
 
 
@@ -131,17 +150,17 @@ async def create_arc_meter(datain: schemas.ArcCreateMeter, db: Session = Depends
 # Arc Meta data post link
 @app.post("/arc/metadata")
 async def post_arc_metadata(datain: schemas.ArcMetaData, db: Session = Depends(get_db)):
-    # for data in datain:
-    #     print(data)
+    # Add metadata for a specific leed ID in the system
     crud.create_arc_metadata(db, datain)
     return 200
 
 
 # Arc Keys post link
+# To create a new entry in the system if you have access token, refresh token, and time with you ready
+# Different for different clients
 @app.post("/arc/keys")
 async def post_arc_key(datain: schemas.ArcKeyTable, db: Session = Depends(get_db)):
-    # for data in datain:
-    #     print(data)
+    #  .
     datain.dict()
     crud.create_arc_keytable(db, datain)
     return 200
@@ -150,8 +169,7 @@ async def post_arc_key(datain: schemas.ArcKeyTable, db: Session = Depends(get_db
 # Arc Meter data post link
 @app.post("/arc/meter")
 async def post_arc_meter(datain: schemas.ArcMeterTable, db: Session = Depends(get_db)):
-    # for data in datain:
-    #     print(data)
+    # Meter data for each Leed Ids
     crud.create_arc_metertable(db, datain)
     return 200
 
@@ -167,7 +185,7 @@ async def update_arc_metadata(datain: schemas.ArcMetaData, db: Session = Depends
 # Arc data posting link
 @app.get("/arc/consumption")
 async def get_consumption(db: Session = Depends(get_db)):
-    return get_meter_list(db=db, leed_id="8000038023", client_name="burberry")
+    return get_meter_list(db=db, leed_id="8000038413", client_name="magna")
     # return get_meter_consumption_detail(db=db, leed_id="8000037879", meter_id="11843135", meter_number="157798271", client_name="burberry")
     # return get_asset_aggregated_data(data_endpoint="electricity", leed_id="8000037879", start_date="2020-08-29", end_date="2017-08-30", unit="kWh")
     # return get_asset_comprehensive_score(leed_id="8000037879", date="2021-08-11")
